005135e51a8f13545ad046406ee9053356220229
Divyasri1295 <142223021+Divyasri1295@users.noreply.github.com>
2025-11-08 16:25:32 +0530

    Fix formatting issue in redirect_uri assignment

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -90,7 +90,7 @@ def authorized():
         result = client.acquire_token_by_authorization_code(
             request.args['code'],
             scopes=Config.SCOPE,
-            redirect_uri=url_for('authorized', _external=True, _scheme='https')
+            redirect_uri=url_for('authorized', _external=True, _scheme='https'))
         if "error" in result:
             return render_template("auth_error.html", result=result)
         session["user"] = result.get("id_token_claims")
